2d3a4e2ab507433d225da15b8f9b923e05faf480
Kalyan Tarafdar <94344890+kraxgm01@users.noreply.github.com>
2024-08-20 18:33:26 +0530

    Update invoke.sh

--- invoke.sh
+++ invoke.sh
@@ -12,21 +12,30 @@ then
 else
     echo "Brew is not installed"
     #install brew
-    /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    # add to path
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$(whoami)/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    echo ""
+    echo "Brew installed successfully"
 fi
 echo ""
 
 
+
+
+#checking python version
+echo "Installing python 🐍"
+#install python
+brew install python
+#export path
+export PATH="$(brew --prefix python)/libexec/bin:$PATH"
+echo ""
 #checking python version
 echo "Checking Python version 🐍"
-if which python3 > /dev/null 2>&1
-then
-    echo "Python version: $(python --version 2>&1)"
-    echo "Python is installed"
-else
-    echo "Python is not installed"
-    #install python
-fi
+echo "Python version: $(python --version 2>&1)"
+echo ""
+echo "Python installed successfully"
 echo ""
 
 #checking pip version
@@ -36,9 +45,91 @@ then
     echo "Pip version: $(pip --version 2>&1)"
     echo "Pip is installed"
 else
-    echo "Pip is not installed"
+    echo "Error: Pip is not installed"
+    #exit
+    exit 1
 fi
 echo ""
+
+#checking git version
+echo "Checking Git version 📦"
+if which git > /dev/null 2>&1
+then
+    echo "Git version: $(git --version 2>&1)"
+    echo "Git is installed"
+else
+    echo "Git is not installed"
+    #install git
+    brew install git
+    echo ""
+    echo "Git installed successfully"
+fi
+echo ""
+
+#download the file
+echo "getting the file 🗂️"
+#checking if file already exists
+if [ -f the_minimalist.zip ]; then
+    echo "File already exists"
+else
+    echo "File does not exist"
+    #download the file
+    url="https://rrrandombuck.s3.ap-south-1.amazonaws.com/the_minimalist.zip"
+    curl -O "$url"
+    echo "File downloaded successfully"
+fi
+echo ""
+
+#unzip the file
+echo "Unzipping the file 🗜️"
+unzip the_minimalist.zip
+echo "File unzipped successfully"
+echo ""
+
+#checking if virtualenv is installed
+echo "Checking Virtualenv version 📦"
+if which virtualenv > /dev/null 2>&1
+then
+    echo "Virtualenv version: $(virtualenv --version 2>&1)"
+    echo "Virtualenv is installed"
+else
+    echo "Virtualenv is not installed"
+    #install virtualenv
+    brew install virtualenv
+    echo ""
+    echo "Virtualenv installed successfully"
+fi
+echo ""
+
+# setting up the project
+echo "Setting up the project 🚀"
+
+cd the_minimalist
+cd backend
+#creating virtual environment
+echo "Creating virtual environment 📦"
+python -m venv env
+echo "Virtual environment created successfully"
+echo ""
+
+#activating virtual environment
+echo "Activating virtual environment 📦"
+source env/bin/activate
+echo "Virtual environment activated successfully"
+echo ""
+
+#installing requirements
+echo "Installing requirements 📦"
+cd ..
+pip install -r requirements.txt
+pip install flask-cors
+echo "Requirements installed successfully"
+echo ""
+
+#running the server
+echo "Running the server 🚀"
+python backend/app.py
+
 
 
 
