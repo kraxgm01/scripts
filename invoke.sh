a4469489203a497c4c9cf2507ea740702ae41310
Kalyan Tarafdar <94344890+kraxgm01@users.noreply.github.com>
2024-08-20 19:15:10 +0530

    Update invoke.sh

--- invoke.sh
+++ invoke.sh
@@ -108,33 +108,7 @@ else
 fi
 echo ""
 
-# setting up the project
-echo "Setting up the project 🚀"
 
-cd the_minimalist
-cd backend
-#creating virtual environment
-echo "Creating virtual environment 📦"
-python3 -m venv env
-echo "Virtual environment created successfully"
-echo ""
-
-#activating virtual environment
-echo "Activating virtual environment 📦"
-source env/bin/activate
-echo "Virtual environment activated successfully"
-echo ""
-
-#installing requirements
-echo "Installing requirements 📦"
-cd ..
-pip install -r requirements.txt
-echo "Requirements installed successfully"
-echo ""
-
-#running the server
-echo "Running the server 🚀"
-python backend/app.py
 
 
 
