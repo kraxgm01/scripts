ddb1e61fe665f430900d794655222119f965ab63
Kalyan Tarafdar <94344890+kraxgm01@users.noreply.github.com>
2024-08-21 11:31:16 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,33 +3,27 @@
 # setting up the project
 echo "Setting up the project 🚀"
 
-#install python tkinter
-echo "Installing Tkinter 🃏"
-brew install python-tk@3.12
-echo "Installed python-tk"
-echo ""
-
-cd the_minimalist
-cd backend
+# cd the_minimalist
+# cd backend
 #creating virtual environment
 echo "Creating virtual environment 📦"
-python3 -m venv env
+python3 -m venv the_minimalist/backend/env
 echo "Virtual environment created successfully"
 echo ""
 
 #activating virtual environment
 echo "Activating virtual environment 📦"
-source env/bin/activate
+source the_minimalist/backend/env/bin/activate
 echo "Virtual environment activated successfully"
 echo ""
 
 #installing requirements
 echo "Installing requirements 📦"
 cd ..
-pip3 install -r requirements.txt
+pip install -r the_minimalist/requirements.txt
 echo "Requirements installed successfully"
 echo ""
 
 #running the server
 echo "Running the server 🚀"
-python backend/app.py
+python the_minimalist/backend/app.py
