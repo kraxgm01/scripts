9c23c71f07c374ca4b9cbdf36c13bb861209e8fa
Kalyan Tarafdar <94344890+kraxgm01@users.noreply.github.com>
2024-08-23 13:06:06 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,23 +7,23 @@ echo "Setting up the project 🚀"
 # cd backend
 #creating virtual environment
 echo "Creating virtual environment 📦"
-virtualenv the_minimalist/backend/env
+virtualenv $PWD/the_minimalist/backend/env
 echo "Virtual environment created successfully"
 echo ""
 
 #activating virtual environment
 echo "Activating virtual environment 📦"
-source the_minimalist/backend/env/bin/activate
+source $PWD/the_minimalist/backend/env/bin/activate
 echo "Virtual environment activated successfully"
 echo ""
 
 #installing requirements
 echo "Installing requirements 📦"
 cd ..
-pip3 install -r the_minimalist/requirements.txt
+pip3 install -r $PWD/the_minimalist/requirements.txt
 echo "Requirements installed successfully"
 echo ""
 
 #running the server
 echo "Running the server 🚀"
-python3 the_minimalist/backend/app.py
+python3 $PWD/the_minimalist/backend/app.py
